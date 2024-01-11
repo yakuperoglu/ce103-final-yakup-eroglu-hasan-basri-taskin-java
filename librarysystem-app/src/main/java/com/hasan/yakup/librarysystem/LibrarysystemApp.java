86f42a2fcdc8b4294ac8bab2b6fe8e2855730293
HBasriTaskin <taskin.hsnbasri@gmail.com>
2024-01-11 21:25:38 +0300

    Main functions created

--- librarysystem-app/src/main/java/com/hasan/yakup/librarysystem/LibrarysystemApp.java
+++ librarysystem-app/src/main/java/com/hasan/yakup/librarysystem/LibrarysystemApp.java
@@ -1,26 +1,35 @@
+/**
+ * @file LibrarysystemApp.java
+ * @brief Main class to start the Library System application.
+ * 
+ * This class contains the main method to initialize the Library System.
+ * It creates a Scanner for user input, initializes a Librarysystem instance,
+ * and invokes the mainMenu method to start the application.
+ * 
+ * @author Hasan Yakup
+ * @version 1.0
+ * @since 2024-01-11
+ */
 package com.hasan.yakup.librarysystem;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Scanner;
 
 public class LibrarysystemApp {
-
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		Librarysystem librarysystem = new Librarysystem(System.in, System.out);
-		String pathFileBooks = "books.bin";
-		String pathFileUsers = "users.bin";
-		
-		librarysystem.addBook("Deneme",pathFileBooks);
-		List<Book> books = librarysystem.loadBooks(pathFileBooks);
-		for (Book book : books) {
-			System.out.println(book.getId());
-			System.out.println(book.getName());
-			System.out.println(book.isMarked());
-			System.out.println(book.isWishlist());
-			System.out.println(book.isLoaned());
-		}
-
-	}
-
+     /**
+     * The main entry point for the Library System application.
+     * 
+     * @param args Command-line arguments (not used).
+     * @throws FileNotFoundException If a required file is not found.
+     * @throws IOException           If an I/O error occurs.
+     * @throws InterruptedException  If a thread is interrupted during execution.
+     */
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
+        Scanner inputScanner = new Scanner(System.in);
+        Librarysystem librarysystem = new Librarysystem(inputScanner, System.out);
+        String pathFileBooks = "books.bin";
+        String pathFileUsers = "users.bin";
+        librarysystem.mainMenu(pathFileUsers, pathFileBooks);
+    }
 }
