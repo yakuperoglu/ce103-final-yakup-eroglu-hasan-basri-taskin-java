4cfb26be4d9c182b1b409587c7340432ec33a2fe
HBasriTaskin <taskin.hsnbasri@gmail.com>
2024-01-11 22:10:09 +0300

    last doxygen commits
    last

--- librarysystem-app/src/main/java/com/hasan/yakup/librarysystem/Book.java
+++ librarysystem-app/src/main/java/com/hasan/yakup/librarysystem/Book.java
@@ -1,15 +1,68 @@
+/**
+ * The {@code librarysystem} package contains classes related to the library system application.
+ * <p>
+ * This package includes entities such as {@link User} and {@link Book}, representing users and books
+ * in the library system, respectively.
+ * </p>
+ * <p>
+ * Classes in this package are designed to provide functionality for managing library resources,
+ * user accounts, and other aspects of a library system.
+ * </p>
+ * 
+ * @author Hasan Yakup
+ * @version 1.0
+ * @since 2024-01-11
+ */
 package com.hasan.yakup.librarysystem;
 
+/**
+ * The {@code Book} class represents a book in the library system with
+ * attributes such as ID, name, marked status, wishlist status, and loaned status.
+ * <p>
+ * This class provides methods to access and modify the book's information.
+ * </p>
+ * 
+ * @author Hasan Yakup
+ * @version 1.0
+ * @since 2024-01-11
+ */
 public class Book {
+     /**
+     * The unique identifier of the book.
+     */
     private int id;
+    /**
+     * The name/title of the book.
+     */
     private String name;
+    /**
+     * Indicates whether the book is marked by the user.
+     */
     private boolean isMarked;
+      /**
+     * Indicates whether the book is in the user's wishlist.
+     */
     private boolean isWishlist;
+    /**
+     * Indicates whether the book is currently loaned.
+     */
     private boolean isLoaned;
 
+    /**
+     * Default constructor for the {@code Book} class.
+     */
     public Book() {
     }
 
+     /**
+     * Parameterized constructor for the {@code Book} class.
+     *
+     * @param bookId     The unique identifier of the book.
+     * @param bookName   The name/title of the book.
+     * @param marked     Indicates whether the book is marked by the user.
+     * @param wishlist   Indicates whether the book is in the user's wishlist.
+     * @param loaned     Indicates whether the book is currently loaned.
+     */
     public Book(int bookId, String bookname, boolean isMarked, boolean isWishlist, boolean isLoaned) {
         this.id = bookId;
         this.name = bookname;
@@ -18,42 +71,92 @@ public class Book {
         this.isLoaned = isLoaned;
     }
 
+    /**
+     * Gets the unique identifier of the book.
+     *
+     * @return The unique identifier of the book.
+     */
     public int getId() {
         return id;
     }
 
+     /**
+     * Sets the unique identifier of the book.
+     *
+     * @param id The new unique identifier to be set.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+     /**
+     * Gets the name/title of the book.
+     *
+     * @return The name/title of the book.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+ * The {@code Book} class represents a book in the library system with
+ * attributes such as ID, name, marked status, wishlist status, and loaned status.
+ * <p>
+ * This class provides methods to access and modify the book's information.
+ * </p>
+ * 
+ * @author Hasan Yakup
+ * @version 1.0
+ * @since 2024-01-11
+ */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Indicates whether the book is marked by the user.
+     */
     public boolean isMarked() {
         return isMarked;
     }
 
+     /**
+     * The name/title of the book.
+     */
     public void setMarked(boolean marked) {
         isMarked = marked;
     }
 
+     /**
+     * Indicates whether the book is in the user's wishlist.
+     */
     public boolean isWishlist() {
         return isWishlist;
     }
 
+    /**
+    * Sets the wishlist status of the book.
+    *
+    * @param wishlist The new wishlist status to be set.
+    */
     public void setWishlist(boolean wishlist) {
         isWishlist = wishlist;
     }
 
+    /**
+     * Checks if the book is currently loaned.
+     *
+     * @return {@code true} if the book is loaned, {@code false} otherwise.
+     */
     public boolean isLoaned() {
         return isLoaned;
     }
 
+    /**
+     * Sets the loaned status of the book.
+     *
+     * @param loaned The new loaned status to be set.
+     */
     public void setLoaned(boolean loaned) {
         isLoaned = loaned;
     }
--- librarysystem-app/src/main/java/com/hasan/yakup/librarysystem/User.java
+++ librarysystem-app/src/main/java/com/hasan/yakup/librarysystem/User.java
@@ -1,23 +1,75 @@
+/**
+ * The {@code librarysystem} package contains classes related to the library system application.
+ * <p>
+ * This package includes entities such as {@link User} representing users in the library system.
+ * </p>
+ * <p>
+ * Classes in this package are designed to provide functionality for managing library resources,
+ * user accounts, and other aspects of a library system.
+ * </p>
+ * 
+ * @author Hasan Yakup
+ * @version 1.0
+ * @since 2024-01-11
+ */
 package com.hasan.yakup.librarysystem;
-
+/**
+ * The {@code User} class represents a user in the library system with
+ * attributes such as email and password.
+ * <p>
+ * This class provides methods to access and modify the user's email and password.
+ * </p>
+ * 
+ * @author Hasan Yakup
+ * @version 1.0
+ * @since 2024-01-11
+ */
 public class User {
+
+    /**
+     * The email address of the user.
+     */
 	private String email;
+
+     /**
+     * The password of the user.
+     */
     private String password;
 
-    // Getter and Setter methods go here
+
+    /**
+     * Gets the email address of the user.
+     *
+     * @return The email address of the user.
+     */
 
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email address of the user.
+     *
+     * @param email The new email address to be set.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets the password of the user.
+     *
+     * @return The password of the user.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password of the user.
+     *
+     * @param password The new password to be set.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
