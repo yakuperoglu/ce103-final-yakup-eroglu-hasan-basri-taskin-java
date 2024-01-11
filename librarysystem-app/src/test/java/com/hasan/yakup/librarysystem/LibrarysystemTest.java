f705d1497f0f12ecc5289fef89135b9dc1de7411
HBasriTaskin <taskin.hsnbasri@gmail.com>
2024-01-11 21:26:53 +0300

    main menu tests were craeted

--- librarysystem-app/src/test/java/com/hasan/yakup/librarysystem/LibrarysystemTest.java
+++ librarysystem-app/src/test/java/com/hasan/yakup/librarysystem/LibrarysystemTest.java
@@ -59,5 +59,45 @@ public class LibrarysystemTest {
 
         assertEquals(1, newId);
     }
-    
+
+    @Test
+    public void testLoadBooks_ShouldLoadBooksFromFile() throws FileNotFoundException, IOException {
+        createTestFile();
+
+        Librarysystem library = new Librarysystem(new Scanner(""), new PrintStream(outContent));
+        List<Book> books = library.loadBooks(testFilePathBooks);
+
+        assertNotNull(books);
+        assertEquals(4, books.size());
+    }
+
+    @Test
+    public void testHandleInputError_ShouldReturnFalse() {
+        InputStream in = new ByteArrayInputStream("".getBytes());
+        Scanner testScanner = new Scanner(in);
+
+        Librarysystem library = new Librarysystem(testScanner, new PrintStream(outContent));
+
+        boolean result = library.handleInputError();
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testMain_Menu_ShouldVisitAllFunctionsAndExit() throws InterruptedException, IOException {
+        // String input = "2\nexample\nexample\n\n3\n2\n1\nexample\nexample\n\n5\n4";
+        String input = "2\nexample\nexample\n\n1\nexample\nexample\n\n5\n3\n2\n123\n\n4\n";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner testScanner = new Scanner(in);
+
+        Librarysystem library = new Librarysystem(testScanner, new PrintStream(outContent));
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        int result = library.mainMenu(testFilePathUsers, testFilePathBooks);
+        assertEquals(0, result);
+    }
+
 }
